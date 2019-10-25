95594aef13e85697160dccf80fd9d9561c5d95be
TonyWu <wu.yong@thelittlegym.com.cn>
2019-10-25 13:57:39 +0800

    tip for ots progress

--- pages/TipAll.vue
+++ pages/TipAll.vue
@@ -1,7 +1,7 @@
 <template>
    <div>
         <component :select="select" :is="task"></component>
-        <Tip-for-pd></Tip-for-pd>
+        <!-- <Tip-for-pd></Tip-for-pd> -->
         <modal v-for="indx of indexes" :show.sync="indx.warn" effect="fade" >
             <div slot="title" class="modal-title">
                 <span class="glyphicon glyphicon-info-sign text-danger"></span>
